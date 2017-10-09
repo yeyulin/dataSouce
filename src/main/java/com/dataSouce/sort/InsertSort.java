5166a35a1ea1132e02159f10b2ca721250ac7571
yeyulin <ityeyulin@163.com>
2017-10-09 15:10:59 +0800

    add InsertSort

---
+++ src/main/java/com/dataSouce/sort/InsertSort.java
@@ -0,0 +1,33 @@
+package com.dataSouce.sort;
+
+import java.util.Arrays;
+
+import com.dataSouce.test.SortTestHelper;
+
+public class InsertSort {
+	public static void insertSort(int arr[]) {
+		int length=arr.length;
+		for(int i=1;i<length;i++) {
+			for(int j=i;j>0 && arr[j]<arr[j-1];j--) {
+				SortTestHelper.swap(arr, j-1, j);
+			}
+		}
+	}
+	public static void insertSortOptimize(int arr[]) {
+		int length=arr.length;
+		for(int i=1;i<length;i++) {
+			int temp=arr[i];
+			int j=i;
+			for(;j>0 && temp<arr[j-1];j--) {
+				arr[j]=arr[j-1];
+			}
+			arr[j]=temp;
+		}
+	}
+	public static void main(String[] args) {
+		int arr[]  = SortTestHelper.generateRandomArray(10, 0, 10);
+		System.out.println(Arrays.toString(arr));
+		SortTestHelper.sortTest("", arr, InsertSort::insertSortOptimize);
+		System.out.println(Arrays.toString(arr));
+	}
+}
