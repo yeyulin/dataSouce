b80ab29f5f80711fee0fe3c6f071729b8daca81d
yeyulin <ityeyulin@163.com>
2017-10-09 10:18:58 +0800

    add selectSort

---
+++ src/main/java/com/dataSouce/sort/SelectSort.java
@@ -0,0 +1,25 @@
+package com.dataSouce.sort;
+
+import java.util.Arrays;
+
+import com.dataSouce.test.SortTestHelper;
+
+public class SelectSort {
+	public static void selectSort(int arr[]) {
+		int length=arr.length;
+		for(int i=0;i<length;i++) {
+			int minIndex=i;
+			for(int j=i+1;j<length;j++) {
+				if(arr[j]<arr[minIndex]) {
+					minIndex=j;
+				}
+			}
+			SortTestHelper.swap(arr, i, minIndex);
+		}
+	}
+	public static void main(String[] args) {
+		int arr[]  = SortTestHelper.generateRandomArray(10, 0, 10);
+		SortTestHelper.sortTest("选择", arr, SelectSort::selectSort);
+		System.out.println(Arrays.toString(arr));
+	}
+}
