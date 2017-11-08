196b9b87df41d620d7574924a20c6cdb447c712e
yeyulin <ityeyulin@163.com>
2017-11-08 15:15:57 +0800

    新添插入方法 递归与非递归

--- src/main/java/com/dataSouce/BinarySearchTree/BinarySearchTreeBasics.java
+++ src/main/java/com/dataSouce/BinarySearchTree/BinarySearchTreeBasics.java
@@ -17,6 +17,31 @@ public class BinarySearchTreeBasics<K extends Comparable<K>,V> {
 			this.left = null;
 			this.right = null;
 		}
+		public K getK() {
+			return k;
+		}
+		public void setK(K k) {
+			this.k = k;
+		}
+		public V getV() {
+			return v;
+		}
+		public void setV(V v) {
+			this.v = v;
+		}
+		public Node getLeft() {
+			return left;
+		}
+		public void setLeft(Node left) {
+			this.left = left;
+		}
+		public Node getRight() {
+			return right;
+		}
+		public void setRight(Node right) {
+			this.right = right;
+		}
+		
 	}
 	private Node root;//树的根节点
 	private int count; // 树种的节点个数
@@ -29,6 +54,90 @@ public class BinarySearchTreeBasics<K extends Comparable<K>,V> {
 	}
 	public int size() {
 		return count;
+	}
+	public void insertRecursion(K k,V v) {
+		//递归插入
+		root=insertRecursion(root,k,v);
+	}
+	//向以node为根的二分搜索树中,插入节点(k,v)使用递归算法,返回插入新节点后的二分搜索树的根
+	private Node insertRecursion(Node root, K k, V v) {
+		if(root==null) {
+			count++;
+			return new Node(k, v);
+		}
+		if(root.getK().compareTo(k)==0) {
+			root.setV(v);
+		}else if(k.compareTo(root.getK())>0) {
+			root.setRight(insertRecursion(root.getRight(), k, v));
+		}else {
+			root.setLeft(insertRecursion(root.getLeft(), k, v));
+		}
+		return root;
+	}
+	public void insertLoop(K k,V v) {
+		if(root==null) {
+			root=new Node(k, v);
+			count++;
+			return;
+		}
+		Node parent=null;//最后一个节点
+		Node node=root;
+		int cmp=0;
+		//当node为null parent为最后一个节点时退出循环 或更新节点值时退出方法
+		while(node!=null) {
+			parent=node;
+			cmp=k.compareTo(node.getK());
+			if(cmp>0) {
+				//把右节点赋值为当前节点
+				node=node.getRight();
+			}else if(cmp<0) {
+				//把左节点赋值为当前节点
+				node=node.getLeft();
+			}else {
+				//更新当前节点值
+				node.setV(v);
+				return;
+			}
+		}
+		if(k.compareTo(parent.getK())>0) {
+			parent.setRight(new Node(k, v));
+		}else {
+			parent.setLeft(new Node(k, v));
+		}
+		count++;
+	}
+	 // 在二分搜索树中搜索键key所对应的值。如果这个值不存在, 则返回null
+    public V search(K key){
+        return search( root , key );
+    }
+	  // 在以node为根的二分搜索树中查找key所对应的value
+    // 若value不存在, 则返回NULL
+    private V search(Node node, K key){
+
+        if( node == null )
+            return null;
+
+        if( key.compareTo(node.getK()) == 0 ) {
+            return node.getV();
+        }
+        else if( key.compareTo(node.getK()) < 0 ) {
+            return search( node.getLeft() , key );
+        }
+        else { // key > node->key{
+            return search( node.getRight(), key );
+        }
+    }
+	public Node getRoot() {
+		return root;
+	}
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
 	}
 	
 }
