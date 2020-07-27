a77e47be5b23eaa90dc47086ac1ceecb514ca783
ityeyulin@163.com <ityeyulin@163.com>
2020-07-27 18:56:38 +0800

    add MergeSort.java

--- src/main/java/com/dataSouce/timbersaw/sort/MergeSort.java
+++ src/main/java/com/dataSouce/timbersaw/sort/MergeSort.java
@@ -6,7 +6,11 @@ import java.util.Arrays;
 
 /**
  * @author yeyulin
- * @description:
+ * @description: 含义：直接在原序列上直接归并排序，每次归并排序分别对左右两边进行归并排序，直至细分到两两分组
+ * 平均时间复杂度：O(nlogn)
+ * 最坏时间复杂度：O(nlogn)
+ * 最优时间复杂度：O(nlogn)
+ * 最坏空间复杂度：O(n)
  * @date 2020/7/27 9:08
  **/
 public class MergeSort {
@@ -86,32 +90,82 @@ public class MergeSort {
      */
     private void mMerge(int[] arr, int left, int mid, int right) {
         int[] aux = Arrays.copyOfRange(arr, left, right + 1);
-        int li=left;
-        int ri=mid+1;
-        for (int i = left; i <=right ; i++) {
-            if(li>mid){
-                arr[i]=aux[ri-left];
+        int li = left;
+        int ri = mid + 1;
+        for (int i = left; i <= right; i++) {
+            if (li > mid) {
+                arr[i] = aux[ri - left];
                 ri++;
-            }else if(ri>right){
-                arr[i]=aux[li-left];
+            } else if (ri > right) {
+                arr[i] = aux[li - left];
                 li++;
-            }
-            else if(aux[li-left]>aux[ri-left]){
-                arr[i]=aux[ri-left];
+            } else if (aux[li - left] > aux[ri - left]) {
+                arr[i] = aux[ri - left];
                 ri++;
-            }else{
-                arr[i]=aux[li-left];
+            } else {
+                arr[i] = aux[li - left];
                 li++;
             }
         }
     }
 
+    public void mergeSort3(int arr[]) {
+        SortTestHelper.prefix(arr);
+        morge3(arr, 0, arr.length-1);
+    }
+
+    private void morge3(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left / 2 + right / 2;
+        morge3(arr, left, mid);
+        morge3(arr, mid + 1, right);
+        if (arr[mid] > arr[mid + 1]) {
+            morge3(arr, left, mid, right);
+        }
+
+    }
+
+    private void morge3(int[] arr, int left, int mid, int right) {
+        int[] aux = Arrays.copyOfRange(arr, left, right + 1);
+        //这时有两个数组 一个是aux 一个是arr，aux是arr要排序截取的一部分，对该部分排序，赋值给arr
+        //aux又mid分割为两个数组，aux[left...mid] aux[mid+1..right]
+        //这两个数组进行排序，最后赋值到arr
+        int li = left;
+        int ri = mid + 1;
+
+        for (int i = left; i <= right; i++) {
+            //当存在左边完全大于右边时，右边数据已经被赋值，左边数据没有被赋值
+            if (ri > right) {
+                arr[i] = aux[li - left];
+                li++;
+
+            } else if (li > mid) {
+                //当存右边完全大于左边时，左边数据已经被赋值，右边数据没有被赋值
+                arr[i] = aux[ri - left];
+                ri++;
+
+            } else if (aux[li - left] > aux[ri - left]) {
+                arr[i] = aux[ri - left];
+                ri++;
+
+            } else {
+                arr[i] = aux[li - left];
+                li++;
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = SortTestHelper.generateRandomArray(10_000_000, 0, 10_000_000);
+        int arr[] = SortTestHelper.generateRandomArray(1_000_000, 0, 10_000_000);
         MergeSort mergeSort = new MergeSort();
         SortTestHelper.sortTest("归并", arr, mergeSort::sortTest);
-        int arr1[] = SortTestHelper.generateRandomArray(10_000_000, 0, 10_000_000);
-        SortTestHelper.sortTest("归并", arr1, mergeSort::mergeSort2);
+        int arr2[] = SortTestHelper.generateRandomArray(1_000_000, 0, 10_000_000);
+        SortTestHelper.sortTest("归并", arr2, mergeSort::mergeSort2);
+        int arr3[] = SortTestHelper.generateRandomArray(1_000_000, 0, 10_000_000);
+        SortTestHelper.sortTest("归并", arr3, mergeSort::mergeSort3);
         //System.out.println(Arrays.toString(arr));
     }
 
