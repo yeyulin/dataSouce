96d201ca52cf1b5fed95f789bb58d0f31032a800
yeyulin <yeyl5496@joyowo.com>
2020-07-30 16:07:23 +0800

    update LRU cache

--- src/main/java/com/datasouce/timbersaw/cache/LRUCache.java
+++ src/main/java/com/datasouce/timbersaw/cache/LRUCache.java
@@ -16,17 +16,17 @@ public class LRUCache {
     }
 
     public Object get(Object key) {
-        if (lruCacheMap.containsKey(key)) {
-            Object value = lruCacheMap.get(key);
-            //先删除，然后重新移除
-            lruCacheMap.remove(key);
-            lruCacheMap.put(key, value);
-            return value;
-        }
-        return null;
+//        if (lruCacheMap.containsKey(key)) {
+//            Object value = lruCacheMap.get(key);
+//            //先删除，然后重新移除
+//            lruCacheMap.remove(key);
+//            lruCacheMap.put(key, value);
+//            return value;
+//        }
+        return lruCacheMap.get(key);
     }
 
-    public void set(Object key, Object value) {
+    public void put(Object key, Object value) {
         lruCacheMap.put(key, value);
     }
 
@@ -42,18 +42,14 @@ public class LRUCache {
 
     public static void main(String[] args) {
         LRUCache cache = new LRUCache(3);
-        cache.set("1", "1");
-        cache.set("2", "2");
-        cache.set("3", "3");
-
-        Object o = cache.get("4");
+        cache.put("1", "A");
+        cache.put("2", "B");
+        cache.put("3", "C");
         cache.get("1");
-        cache.get("2");
+        cache.put("4", "D");
         cache.entrySet();
-        //3-1-2
-        //1-2-4
-        cache.set("4", "4");
-
+        cache.get("3");
+        cache.put("5","E");
         cache.entrySet();
     }
 }
